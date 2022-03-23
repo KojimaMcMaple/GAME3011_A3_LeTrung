9c6586aae4f1e011ca1d018f003a44744641187f
KojimaMcMaple <KyleHunter.KH@outlook.com>
2022-03-23 13:37:48 -0400

    allow grid to support custom obj

--- GAME3011_A3_LeTrung/Assets/Scripts/Grid.cs
+++ GAME3011_A3_LeTrung/Assets/Scripts/Grid.cs
@@ -1,38 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid
+public class Grid<T>
 {
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
     private int width_;
     private int height_;
     private float cell_size_;
     private Vector3 origin_;
-    private int[,] grid_arr_;
-    private TextMesh[,] debug_text_arr_;
+    private T[,] grid_arr_;
 
-    public Grid(int width, int height, float cell_size, Vector3 origin)
+    public Grid(int width, int height, float cell_size, Vector3 origin,
+        Func<Grid<T>, int, int, T> InitGridObj) //pass in Grid, x, y
     {
         this.width_ = width;
         this.height_ = height;
         this.cell_size_ = cell_size;
         this.origin_ = origin;
 
-        grid_arr_ = new int[width, height];
-        debug_text_arr_ = new TextMesh[width, height];
+        grid_arr_ = new T[width, height];
 
+        // INIT WITH DEFAULT VALUE
         for (int x = 0; x < grid_arr_.GetLength(0); x++)
         {
             for (int y = 0; y < grid_arr_.GetLength(1); y++)
             {
-                Vector3 offset = new Vector3(cell_size_, cell_size_) * 0.5f; //offset so texts don't stay at bottom left
-                debug_text_arr_[x,y] = CreateWorldText(grid_arr_[x, y].ToString(), null, GetWorldPos(x, y) + offset, 20, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPos(x, y), GetWorldPos(x, y + 1), Color.green, 100f);
-                Debug.DrawLine(GetWorldPos(x, y), GetWorldPos(x + 1, y), Color.magenta, 100f);
+                grid_arr_[x, y] = InitGridObj(this, x, y);
             }
         }
-        Debug.DrawLine(GetWorldPos(0, height_), GetWorldPos(width_, height_), Color.red, 100f);
-        Debug.DrawLine(GetWorldPos(width_, 0), GetWorldPos(width_, height_), Color.blue, 100f);
+
+        // DEBUG
+        bool is_debug = true;
+        if (is_debug)
+        {
+            TextMesh[,] debug_text_arr = new TextMesh[width, height];
+            for (int x = 0; x < grid_arr_.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid_arr_.GetLength(1); y++)
+                {
+                    Vector3 offset = new Vector3(cell_size_, cell_size_) * 0.5f; //offset so texts don't stay at bottom left
+                    debug_text_arr[x, y] = CreateWorldText(grid_arr_[x, y]?.ToString(), //null check grid_arr_[x, y]? 
+                        null, GetWorldPos(x, y) + offset, 20, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPos(x, y), GetWorldPos(x, y + 1), Color.green, 100f);
+                    Debug.DrawLine(GetWorldPos(x, y), GetWorldPos(x + 1, y), Color.magenta, 100f);
+                }
+            }
+            Debug.DrawLine(GetWorldPos(0, height_), GetWorldPos(width_, height_), Color.red, 100f);
+            Debug.DrawLine(GetWorldPos(width_, 0), GetWorldPos(width_, height_), Color.blue, 100f);
+
+            OnGridObjectChanged += (sender, args) => { 
+                debug_text_arr[args.x, args.y].text = grid_arr_[args.x, args.y]?.ToString(); //lamda func so you don't have to define a big function below
+            };
+        }
+    }
+
+    public void DoTriggerGridObjChanged(int x, int y)
+    {
+        OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, y = y });
     }
 
     private Vector3 GetWorldPos(int x, int y) //GetCellWorldPos
@@ -47,27 +79,31 @@ public class Grid
         return new Vector2Int(x, y);
     }
 
-    public void SetValue(int x, int y, int value) //SetCellValue
+    public void SetValue(int x, int y, T value) //SetCellValue
     {
         if (x<0 || y<0 || x>=width_ && y>=height_)
         {
             return;
         }
         grid_arr_[x, y] = value;
-        debug_text_arr_[x, y].text = grid_arr_[x, y].ToString();
+        if (OnGridObjectChanged != null)
+        {
+            OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+        }
+        //debug_text_arr_[x, y].text = grid_arr_[x, y].ToString();
     }
 
-    public void SetValue(Vector3 world_pos, int value) //SetCellValue
+    public void SetValue(Vector3 world_pos, T value) //SetCellValue
     {
         Vector2Int coords = GetGridCoords(world_pos);
         SetValue(coords.x, coords.y, value);
     }
 
-    public int GetValue(int x, int y)
+    public T GetValue(int x, int y)
     {
         if (x < 0 || y < 0 || x >= width_ && y >= height_)
         {
-            return 0;
+            return default(T); //C# default value of type T
         }
         else
         {
@@ -75,7 +111,7 @@ public class Grid
         }
     }
 
-    public int GetValue(Vector3 world_pos)
+    public T GetValue(Vector3 world_pos)
     {
         Vector2Int coords = GetGridCoords(world_pos);
         return GetValue(coords.x, coords.y);
